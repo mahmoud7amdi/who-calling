cd955463bd28418ee813ddc86ba93ee95974ace3
unknown <mahmoudhamdyam@gmail.com>
2023-11-29 16:59:10 +0200

    contacts first

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,185 +9,67 @@ use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
-    public function search(Request $request)
+    public function Search(Request $request)
     {
-        $contacts = DB::table('contacts')->select('name','phone');
+        $contacts = Contacts::select('name', 'phone');
 
-        $user = DB::table('users')->select('id', DB::raw('CONCAT(first_Name, " ", last_Name) AS name'), 'phone', 'email',
+        $user = User::select('id', 'phone', DB::raw('CONCAT(first_Name, " ", last_Name) AS name'), 'email',
             'company', 'job_description', 'profile_image', 'lat', 'long', 'type', 'last_seen',
             'country_id', 'country_code'
         );
 
-        if ($contacts)
-        {
-            if ($request->phone && $request->country_id){
-             $result = $contacts->where('phone', $request->phone)
-                 ->where('country_id', $request->country_id)->get();
-            }
-            return response()->json([
-            'status' => 200,
-            "message" => 'All search',
-            "data" => $result
-        ]);
-        }elseif ($user)
-        {
-            if ($request->phone && $request->country_id){
-                $result = $user->where('phone', $request->phone)
+        if ($request->phone && $request->country_id) {
+
+
+            $result = $contacts->where('phone', $request->phone)
+                ->where('country_id', $request->country_id)->get();
+
+            if ($result->isEmpty()) {
+                $value = $user->where('phone', $request->phone)
                     ->where('country_id', $request->country_id)->get();
+                return response()->json([
+                    "message" => 'All search',
+                    "data" => $value
+                ]);
+
             }
             return response()->json([
-                'status' => 200,
                 "message" => 'All search',
                 "data" => $result
+            ]);
+
+
+        } elseif ($request->name && $request->country_id) {
+            $result = $contacts->where('name', $request->name)
+                ->where('country_id', $request->country_id)->get();
+            if ($result->isEmpty()) {
+
+                $value = $user->where(DB::raw("concat(first_name, ' ', last_name) "), $request->name)
+                    ->where('country_id', $request->country_id)->get();
+                return response()->json([
+                    "message" => 'All search',
+                    "data" => $value
                 ]);
+            }
+            return response()->json([
+                "message" => 'All search',
+                "data" => $result
+            ]);
+
+
+        }elseif ($request->phone && $request->country_id && $request->first_name){
+            $result = $user->where('phone', $request->phone)
+                ->where('country_id',$request->country_id)
+                ->where('first_name',$request->first_name)->get();
+            return response()->json([
+                "message" => 'All search',
+                "data" => $result
+            ]);
+        }else{
+            return response()->json([
+                "message" => 'Not Found'
+            ]);
         }
 
-
-
-
-    }}
-
-
-//        if ($request->name && $request->country_id){
-//             $result = $contacts->where('name', $request->name)
-//                 ->where('country_id', $request->country_id)->get();
-//
-//         }elseif($request->phone && $request->country_id) {
-//             $result = $user->where('phone', $request->phone)
-//                 ->where('country_id', $request->country_id)->get();
-//
-//
-//         }elseif ($request->name && $request->country_id) {
-//            $result = $user->where(DB::raw("concat(first_name, ' ', last_name) like '%name%' "), $request->name)
-//                ->where('country_id', $request->country_id)->get();
-//
-//
-//
-//        }elseif ($request->phone && $request->country_id && $request->name) {
-//            $result = $user->where('phone', $request->phone)
-//                ->where('country_id', $request->country_id)
-//                ->where(DB::raw("concat(first_name, ' ', last_name)"), $request->name)->get();
-//
-//        }elseif ($request->phone && $request->country_id) {
-//            $result = $contacts->where('phone', $request->phone)
-//                ->where('country_id', $request->country_id)->get();
-//
-//        }elseif ($request->name && $request->country_id) {
-//            $result = $contacts->where('name', $request->name)
-//                    ->where('country_id', $request->country_id)->get();
-//
-//        }else{
-//            return 'not found';
-//        }
-//        return response()->json([
-//            'status' => 200,
-//            "message" => 'All search',
-//            "data" => $result
-//        ]);
-
-
-
-//            if ($request->phone && $request->country_id) {
-//                $result = $contacts->where('phone', $request->phone)
-//                    ->where('country_id', $request->country_id)->get();
-//                return response()->json([
-//                    'status' => 200,
-//                    "message" => 'All search',
-//                    "data" => $result
-//                ]);
-//
-//            } elseif ($request->name && $request->country_id) {
-//                $result = $contacts->where('name', $request->name)
-//                    ->where('country_id', $request->country_id)->get();
-//                return response()->json([
-//                    'status' => 200,
-//                    "message" => 'All search',
-//                    "data" => $result
-//                ]);
-//            }
-
-
-
-//        if ($contacts) {
-//
-//            if ($request->phone && $request->country_id) {
-//                $result = $contacts->where('phone', $request->phone)
-//                    ->where('country_id', $request->country_id)->get();
-//                return response()->json([
-//                    'status' => 200,
-//                    "message" => 'All search',
-//                    "data" => $result
-//                ]);
-//
-//            } elseif ($request->fisrt_name && $request->country_id) {
-//                $result = $contacts->where('fisrt_name', $request->fisrt_name)
-//                    ->where('country_id', $request->country_id)->get();
-//                return response()->json([
-//                    'status' => 200,
-//                    "message" => 'All search',
-//                    "data" => $result
-//                ]);
-//
-//
-//            } elseif ($request->phone && $request->country_id && $request->name) {
-//                $result = $contacts->where('phone', $request->phone)
-//                    ->where('country_id', $request->country_id)
-//                    ->where(DB::raw("concat(first_name, ' ', last_name)"), $request->name)->get();
-//                return response()->json([
-//                    'status' => 200,
-//                    "message" => 'All search',
-//                    "data" => $result
-//                ]);
-//            }
-//
-//
-//
-//        }else{
-//            return 'not found';
-//        }
-
-
-//
-//    }
-//}
-
-
-//    public function Search(Request $request)
-//    {
-//        $user =  User::select('id', DB::raw('CONCAT(first_Name, " ", last_Name) AS name'), 'phone','email',
-//            'company','job_description','profile_image','lat','long','type','last_seen',
-//            'country_id','country_code'
-//        );
-//        $value = $request->name;
-//        if ($request->phone && $request->country_id)
-//        {
-//            $result = $user->where('phone', $request->phone)
-//                ->where('country_id',$request->country_id)->get();
-//
-//        }elseif ($request->name && $request->country_id){
-//            $result = $user->where( DB::raw("concat(first_name, ' ', last_name) like '%name%' "), $request->name)
-//                ->where('country_id',$request->country_id)->get();
-//
-//
-//        }elseif ($request->phone && $request->country_id && $request->name)
-//        {
-//            $result = $user->where('phone', $request->phone)
-//                ->where('country_id',$request->country_id)
-//                ->where(DB::raw("concat(first_name, ' ', last_name)"),$request->name)->get();
-//        }else{
-//            return response()->json([
-//                "message" => 'Not Found',
-//
-//            ]);
-//        }
-//
-//        //        }elseif ($request->first_name && $request->country_id){
-////            $result = $user->where('first_name', $request->first_name)
-////                ->where('country_id',$request->country_id)->get();
-//
-//        return response()->json([
-//            "message" => 'All search',
-//            "data" => $result
-//        ]);
-//    }
-//}
+    }
+}
